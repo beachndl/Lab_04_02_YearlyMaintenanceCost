bd48c7479106c5271aabc53ee07cab94fc35d25f
Daniel Beachnau <beachndl@mail.uc.edu>
2025-02-03 21:46:00 -0500

    Completed Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Maintenance cost of each season
+        double springCost = 446.25;
+        double summerCost = 568.75;
+        double fallCost = 861.85;
+        double winterCost = 564.95;
+
+        // Calculate total yearly cost
+        double totalYear = springCost + summerCost + fallCost + winterCost;
+
+        // Display seasonal costs
+        System.out.println("Spring Maintenance Cost: $" + springCost);
+        System.out.println("Summer Maintenance Cost: $" + summerCost);
+        System.out.println("Fall Maintenance Cost: $" + fallCost);
+        System.out.println("Winter Maintenance Cost: $" + winterCost);
+
+        // Display total yearly cost
+        System.out.println("Total Yearly Maintenance Cost: $" + totalYear);
     }
 }
